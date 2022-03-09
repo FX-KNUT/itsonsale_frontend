579f82a36a5072d30f70de098edd77555fa847f0
KangMin Her <81793748+nicecarrot@users.noreply.github.com>
2022-03-09 16:06:25 +0900

    testSearchForm

--- components/main/sidebar/SearchForm.tsx
+++ components/main/sidebar/SearchForm.tsx
@@ -2,7 +2,31 @@ import React from "react";
 import styles from "../../../styles/main/sidebar/_searchForm.module.scss";
 
 const SearchForm = () => {
-  return <div className={styles.searchFormWrapper}>SearchForm</div>;
+  return (
+    <div className={styles.searchFormWrapper}>
+      <div className={styles.searchFormLeft}>
+        <select className={styles.sales}>
+          <option value="every" selected>
+            전체
+          </option>
+          <option value="oPo">1+1</option>
+          <option value="tPo">2+1</option>
+        </select>
+      </div>
+      <div className={styles.searchFormFill}>
+        <input type="text" name="Product" placeholder=" " />
+        <label className={styles.move} htmlFor="Product">Product</label>
+      </div>
+
+      <div className={styles.searchFormRight}>
+        <input
+          className={styles.searchSubmitButton}
+          id="search-submit-button"
+          type="submit"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchForm;
