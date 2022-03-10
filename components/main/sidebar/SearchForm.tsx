44bb82b0e2ac77ff1788226f593badf999af812f
KangMin Her <81793748+nicecarrot@users.noreply.github.com>
2022-03-10 16:02:02 +0900

    class이름 바꿀거에요

--- components/main/sidebar/SearchForm.tsx
+++ components/main/sidebar/SearchForm.tsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../../styles/main/sidebar/_searchForm.module.scss";
 
 const SearchForm = () => {
+  const [sales, setSales] = useState<String>("전체");
+  const changeSales = (e) => {
+    setSales(e.target.value);
+  };
   return (
     <div className={styles.searchFormWrapper}>
       <div className={styles.searchFormLeft}>
-        <select className={styles.sales}>
-          <option value="every" selected>
+        <select className={styles.sales} onChange={changeSales}>
+          <option value="전체" selected>
             전체
           </option>
-          <option value="oPo">1+1</option>
-          <option value="tPo">2+1</option>
+          <option value="1+1">1+1</option>
+          <option value="2+1">2+1</option>
         </select>
+        <p>
+          <span>{sales}</span>
+          <i className="fa-solid fa-sort-down"></i>
+        </p>
       </div>
       <div className={styles.searchFormFill}>
-        <input className={styles.test} type="text" name="Product" placeholder=" " />
-        <label className={styles.move} htmlFor="Product">Product</label>
+        <input
+          className={styles.test}
+          type="text"
+          name="Product"
+          placeholder=" "
+        />
+        <label className={styles.move} htmlFor="Product">
+          Product
+        </label>
       </div>
 
       <div className={styles.searchFormRight}>
-        <input
-          className={styles.searchSubmitButton}
-          id="search-submit-button"
-          type="submit"
-        />
+          <button
+            className={styles.searchSubmitButton}
+            id="search-submit-button"
+            type="submit"
+          ></button>
+          <i className="fa-solid fa-magnifying-glass"></i>
+
       </div>
     </div>
   );
